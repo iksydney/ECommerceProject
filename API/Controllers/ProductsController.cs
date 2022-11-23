aa5455737353de337ecad0b155112e6adacef79c
ikenna.ogbonna <ikenna.ogbonna@bankly.ng>
2022-11-23 22:52:29 +0100

    class Library and cleanup

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
+using Core.Entities;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -6,9 +9,22 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        [HttpGet]
-        public string GetProducts(){
-            return "list of products";
+        private readonly ApplicationDbContext _store;
+        public ProductsController(ApplicationDbContext store)
+        {
+            _store = store;
+        }
+        [HttpGet("GetAllProducts")]
+        public async Task<ActionResult<List<Product>>> GetProducts(){
+            var products  = await _store.Products.ToListAsync();
+            return products;
+        }
+
+        //[ProducesResponseType(statusCode:StatusCode.Status200OK )]
+        [HttpGet("GetAProduct/{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id){
+            var product = await _store.Products.FindAsync(id);
+            return Ok(product);
         }
         
     }
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
