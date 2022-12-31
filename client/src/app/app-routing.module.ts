f11c106332621dd8d4db7ec7a9b009f071289846
ikenna.ogbonna <ikenna.ogbonna@bankly.ng>
2022-12-31 16:18:37 +0100

    Checkout module and Cart functionality implementations

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -21,6 +21,7 @@ const routes: Routes =
   {path: 'totals', component:OrderTotalsComponent},
   {path:'shop', loadChildren: () => import('./shop/shop.module').then(x => x.ShopModule), data: {breadcrumb:'Shop'}},
   {path:'basket', loadChildren: () => import('./basket/basket.module').then(x => x.BasketModule), data: {breadcrumb:'Basket'}},
+  {path:'checkout', loadChildren: () => import('./checkout/checkout.module').then(x => x.CheckoutModule), data: {breadcrumb:'Checkout'}},
   {path:'**', redirectTo:'not-found',pathMatch:'full'}
 ];
 
--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BasketService } from 'src/app/basket/basket.service';
 import { IProduct } from 'src/app/shared/models/product';
 import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
@@ -11,15 +12,35 @@ import { ShopService } from '../shop.service';
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
+  quantity = 1;
   id: string;
-  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute, private bcService: BreadcrumbService) {
+  constructor(private shopService: ShopService, 
+    private activateRoute: ActivatedRoute, 
+    private bcService: BreadcrumbService, 
+    private basketService: BasketService) {
     bcService.set('ProductDetails',' ');
    }
 
   ngOnInit(): void {
     this.loadProduct();
   }
+
+  adItemToBasket()
+  {
+    this.basketService.addItemToBasket(this.product, this.quantity);
+  }
+
+  incrementQuantity()
+  {
+    this.quantity++;
+  }
   
+  decrementQuantity()
+  {
+    if(this.quantity > 0){
+      this.quantity--;
+    }
+  }
   //idNum = Number(this.activateRoute.snapshot.paramMap.get(`$id`));
   loadProduct(){
     //this.activateRoute.paramMap.subscribe(param => {this.id = param.get('id'); });
