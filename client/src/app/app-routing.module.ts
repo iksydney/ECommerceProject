15f302db33f78d982cf7a06b63a38a1aeb3bc4d7
ikenna.ogbonna <ikenna.ogbonna@bankly.ng>
2022-12-31 15:10:16 +0100

    summary component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BasketComponent } from './basket/basket.component';
 import { NotFoundComponent } from './core/not-found/not-found.component';
 import { SectionHeaderComponent } from './core/section-header/section-header.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
+import { OrderTotalsComponent } from './shared/components/order-totals/order-totals.component';
 import { ProductDetailsComponent } from './shop/product-details/product-details.component';
 import { ShopComponent } from './shop/shop.component';
 
@@ -16,6 +18,7 @@ const routes: Routes =
   {path: 'not-found', component:NotFoundComponent, data: {breadcrumb:'Not Found'}},
   {path: 'server-error', component:ServerErrorComponent, data: {breadcrumb:'Server Error'}},
   {path: 'header', component:SectionHeaderComponent},
+  {path: 'totals', component:OrderTotalsComponent},
   {path:'shop', loadChildren: () => import('./shop/shop.module').then(x => x.ShopModule), data: {breadcrumb:'Shop'}},
   {path:'basket', loadChildren: () => import('./basket/basket.module').then(x => x.BasketModule), data: {breadcrumb:'Basket'}},
   {path:'**', redirectTo:'not-found',pathMatch:'full'}
