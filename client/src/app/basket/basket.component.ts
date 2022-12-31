c59ea990bfea41401517c2558bdbe042aa46b6a6
ikenna.ogbonna <ikenna.ogbonna@bankly.ng>
2022-12-31 14:36:27 +0100

    styling the basket page

--- client/src/app/basket/basket.component.ts
+++ client/src/app/basket/basket.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IBasket } from '../shared/models/basket';
+import { BasketService } from './basket.service';
 
 @Component({
   selector: 'app-basket',
   templateUrl: './basket.component.html',
   styleUrls: ['./basket.component.scss']
 })
-export class BasketComponent {
+export class BasketComponent implements OnInit {
+  
+  constructor(private basketService: BasketService){}
+  basket$ : Observable<IBasket>
+
+  ngOnInit(): void {
+    this.basket$ = this.basketService.basket$;
+  }
 
 }
