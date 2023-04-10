cb6b087e02099d79fc061c77faf4564ec76f321c
ikenna.ogbonna <ikenna.ogbonna@bankly.ng>
2023-04-10 18:06:47 +0100

    address Class

---
+++ Core/Entities/OrderAggregates/Address.cs
@@ -0,0 +1,28 @@
+﻿namespace Core.Entities.OrderAggregates
+{
+    internal class Address
+    {
+        public Address()
+        {
+        }
+
+        public Address(string firstName, string lastName, string street, string city, string state, string postalCode, string country)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Street = street;
+            City = city;
+            State = state;
+            PostalCode = postalCode;
+            Country = country;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+    }
+}
