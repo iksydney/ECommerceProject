43c779d819f8a9ec041b88becea039ce33044378
ikenna.ogbonna <ikenna.ogbonna@bankly.ng>
2023-04-10 18:11:49 +0100

    Delivery method Data Structure

---
+++ Core/Entities/OrderAggregates/DeliveryMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.OrderAggregates
+{
+    public class DeliveryMethod : BaseEntity<int>
+    {
+        public string ShortName { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        public string DeliveryTime { get; set; }
+    }
+}
