5a9b431ad74149bd2dafcfe5dfd0110cd6bec4cc
ikenna.ogbonna <ikenna.ogbonna@bankly.ng>
2023-04-10 18:22:41 +0100

    Supporrting classes for the Order and items

---
+++ Core/Entities/OrderAggregates/OrderStatus.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace Core.Entities.OrderAggregates
+{
+    public enum OrderStatus
+    {
+        [EnumMember(Value = "Pending")]
+        Pending,
+        [EnumMember(Value = "PaymentReceived")]
+        PaymentReceived,
+        [EnumMember(Value = "PaymentFailed")]
+        PaymentFailed,
+        [EnumMember(Value = "OrderShipped")]
+        OrderShipped,
+        [EnumMember(Value = "OrderCancelled")]
+        OrderCancelled
+    }
+}
